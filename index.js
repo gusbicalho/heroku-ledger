fbf86fe209e7bdbe4f01ede5967d327c35771103
Gustavo Bicalho <gusbicalho@gmail.com>
2016-04-06 00:49:45 -0300

    ledger version

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 var express = require('express')
 var app = express()
 
+var ledgerBinary = 'ledger'
+var LedgerCli = require('./node_modules/ledger-cli/lib/cli-runner.js').Cli
+var cli = new LedgerCli(ledgerBinary, {debug: false})
+//var Ledger = require('ledger-cli').Ledger
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  /*
+  var ledger = new Ledger({
+    binary: 'ledger',
+    file: 'temp.dat'
+  })
+  ledger.version((err, version) => response.send(err || version))
+  */
+  var buffer = '';
+  cli.exec(['--version']).stdout.pipe(response.type('txt'));
 })
 
 app.listen(app.get('port'), function() {

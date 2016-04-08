78a746ea1c47439f77897d435262cb7b2ebcf5cb
Gustavo Bicalho <gustavo.bicalho@nubank.com.br>
2016-04-08 09:50:07 -0300

    ad-hoc query to sample

--- app/index.js
+++ app/index.js
@@ -1,28 +1,50 @@
 var express = require('express')
 var app = express()
 var fs = require('fs')
+var escape = require('shell-escape')
+var Cli = require('./cli-runner.js').Cli
 
 var ledgerBinary = 'ledger'
-var LedgerCli = require('./cli-runner.js').Cli
-var cli = new LedgerCli(ledgerBinary)
-//var Ledger = require('ledger-cli').Ledger
+var ledgerCli = new Cli(ledgerBinary)
+
+function execAndPipe(cli, args, stdoutDest, onError) {
+  var proc = cli.exec(args)
+  proc.stdout.pipe(stdoutDest)
+  proc.stderr.on('error', onError)
+}
+
+var wrapInArray = (s) => [s]
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app
-.get('/', function(request, response) {
+.get('/', (req, res) => {
   console.log('GET /')
-  var proc = cli.exec(['--version'])
-  proc.stdout.pipe(response.type('text/plain'));
-  proc.stderr.on('error', (error) => {
-    response.setStatus(500).send(error);
-  })
+  execAndPipe(
+    new Cli(ledgerBinary),
+    ['--version'],
+    res.type('text/plain'),
+    (error) => res.setStatus(500).send(error)
+  )
 })
 .get('/sample/bal', (req, res) => {
   console.log('GET /sample/bal')
-  cli.exec(['-f','sample.dat','bal']).stdout
-  .pipe(res.type('txt'));
+  execAndPipe(
+    new Cli(ledgerBinary),
+    ['-f','sample.dat','bal'],
+    res.type('text/plain'),
+    (error) => res.setStatus(500).send(error)
+  )
+})
+.get('/sample/ad-hoc', (req, res) => {
+  console.log('GET /sample/ad-hoc')
+  execAndPipe(
+    new Cli(ledgerBinary),
+    ['-f','sample.dat'].concat(req.query.q.map(wrapInArray).map(escape)),
+    res.type('text/plain'),
+    (error) => res.setStatus(500).send(error)
+  )
 })
 
 app.listen(app.get('port'), function() {
